1c5d101b44a2140782833bc20a5ae1ef32b32b38
aidanmc95 <aidanmc95@gmail.com>
2020-08-17 13:43:21 -0700

    Add to user serializer

--- app/serializers/simple_user_serializer.rb
+++ app/serializers/simple_user_serializer.rb
@@ -2,6 +2,8 @@ class SimpleUserSerializer < ActiveModel::Serializer
   attributes :id, :username, :about_me, :as_host, :created_at
 
   has_many :boardgames
+  has_many :invites
+  has_many :meets
   class BoardgameSerializer < ActiveModel::Serializer
     attributes :id, :title, :thumbnail, :BGGrating, :BGGrank
   end
